fee56a124f96f06631d12405e7142fd40dd6650b
Christos Pap <decorativepineapple9@gmail.com>
2023-04-18 13:37:55 +0300

    Added VRLadle.sol

--- src/interfaces/IRouter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IRouter {
-    function route(address target, bytes calldata data)
-        external payable
-        returns (bytes memory result);
-}
--- test/mocks/VRCauldronUpgrade.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity >=0.8.13;
-import "../../src/variable/VRCauldron.sol";
-
-contract VRCauldronV2 is VRCauldron {
-    bytes32 public immutable storageCheck = keccak256("alcueca wuz here");
-}
